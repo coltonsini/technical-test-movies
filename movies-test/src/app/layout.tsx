4c78bf9d3888dce8ebdb3a85767687cf228a9444
Juan José Ordoñez Villalobos <49448784+coltonsini@users.noreply.github.com>
2024-12-26 14:32:28 -0500

    Updated the main page layout
    Added the main layout to the page to start working with that

--- movies-test/src/app/layout.tsx
+++ movies-test/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { ibmFont, InterFont } from "./ui/fonts";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "@/components/layout/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${ibmFont.className} ${InterFont.variable} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
