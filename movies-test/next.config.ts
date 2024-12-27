b0d10fa90f24f081ccfb6fb48e05e4917c145b43
Juan José Ordoñez Villalobos <49448784+coltonsini@users.noreply.github.com>
2024-12-26 21:45:24 -0500

    Update files
    Minor updates made

--- movies-test/next.config.ts
+++ movies-test/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['via.placeholder.com', 'image.tmdb.org', 'placehold.co'],
+
+  },
 };
 
 export default nextConfig;
